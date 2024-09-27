c185d99efdc7bc545c1f2cd5f047cca2c8c25673
NViktor77 <nagorniy@ukr.net>
2024-09-27 12:00:11 +0300

    TS-002: Added file loading (#3)
    * TS-002: Added numbers loading
    
    * TS-001: Added print of array (#1)
    
    Co-authored-by: TEST-USER <test@test.com>
    
    * TS-002: Added checking
    
    ---------
    
    Co-authored-by: TEST-USER <test@test.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,46 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws FileNotFoundException {
         System.out.println("Hello!!!!");
+        int[] result = new int[20];
+        loadFromFile(result);
+        print(result);
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; ++i) {
+            System.out.printf("%5d, ", array[i]);
+        }
+        System.out.println();
+    }
+
+    public static void loadFromFile(int[] result) throws FileNotFoundException {
+        URL p = App.class.getResource("/test.txt");
+        if (p == null) {
+            System.out.println("Path to file not found");
+            return;
+        }
+        if (p.getPath() == null) {
+            System.out.println("Path is null");
+            return;
+        }
+        System.out.println(p.getPath());
+        File file = new File(p.getPath());
+        Scanner scanner = new Scanner(file);
+        int i = 0;
+        while (i < result.length && scanner.hasNextInt()) {
+            result[i] = scanner.nextInt();
+            ++i;
+        }
+    }
+
+    public static void sort(int[] orig) {
+
     }
 }
